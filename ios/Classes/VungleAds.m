77211ba1c1b511056f62b516e663d74cd9f5ab2e
sxyberry@gmail.com <5800126866>
2019-08-07 21:57:33 +0800

    init

--- ios/Classes/VungleAds.m
+++ ios/Classes/VungleAds.m
@@ -26,9 +26,18 @@
      return [self.sdk isAdCachedForPlacementID:placementID];
 }
 
-- (BOOL)playAd:(UIViewController *)controller placementID:(nullable NSString *)placementID {
+- (BOOL)playAd:(UIViewController *)controller placementID:(nullable NSString *)placementID userId:(nullable NSString *)userId {
+             NSLog(@"-->> Delegate Callback: uid: %@", userId);
+
      NSError *error;
-    return [self.sdk playAd:controller options:nil placementID:placementID error:&error];
+     NSDictionary *options = @{
+                               VunglePlayAdOptionKeyUser: userId,
+                               VunglePlayAdOptionKeyIncentivizedAlertBodyText : @"关闭视频将会失去奖励，确认关闭视频？",
+                               VunglePlayAdOptionKeyIncentivizedAlertCloseButtonText : @"关闭",
+                               VunglePlayAdOptionKeyIncentivizedAlertContinueButtonText : @"继续观看",
+                               VunglePlayAdOptionKeyIncentivizedAlertTitleText : @"注意!"};
+
+    return [self.sdk playAd:controller options:options placementID:placementID error:&error];
 }
 
 -(void) addRewardVerifyedHandler:(completionHandler)handler {
@@ -54,18 +63,23 @@
 }
 
 - (void)vungleWillCloseAdWithViewInfo:(VungleViewInfo *)info placementID:(NSString *)placementID {
-    NSLog(@"-->> Delegate Callback: vungleWillCloseAdWithViewInfo");
+    NSLog(@"-->> Delegate Callback: vungleWillCloseAdWithViewInfo completedView=%@",info.completedView);
+    if([info.completedView intValue]==1){
+        if(self.completionHandlers!=nil){
+            NSLog(@"奖励开始回调");
+            self.completionHandlers(@"恭喜你，获取金币成功");
+        }else{
+            NSLog(@"注册回调失败");
+        }
+    }else{
+        NSLog(@"没有达到奖励条件");
+    }
 }
 
 - (void)vungleDidCloseAdWithViewInfo:(VungleViewInfo *)info placementID:(NSString *)placementID {
     NSLog(@"-->> Delegate Callback: vungleDidCloseAdWithViewInfo");
     
-    if(self.completionHandlers!=nil){
-            NSLog(@"开始回调");
-            self.completionHandlers(@"恭喜你，获取金币成功");
-    }else{
-            NSLog(@"注册回调失败");
-    }
+    
     if (info) {
         NSLog(@"Info about ad viewed: %@", info);
     }
